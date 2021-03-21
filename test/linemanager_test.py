b76892c74a6481686c5194fe0f874bc242ea7c23
Jitrayu Punrattanapongs <jitrayu.p@gmail.com>
2021-03-22 00:02:24 +0700

    feat: add linemanage test file

---
+++ test/linemanager_test.py
@@ -0,0 +1,9 @@
+from src.linemanager import LineManager
+
+if __name__ == '__main__':
+    token = 'Y484Uowhl0mb5Bmq3wI4Zgfxia81t4giez8jAQByvmc'
+    x = LineManager(token)
+    x.xmarkfilter()
+    x.xmarktdsum()
+    x.xmarkbody()
+
