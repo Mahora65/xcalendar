54765ee70e8f64046dd885edd0f1509d197323de
Jitrayu Punrattanapongs <jitrayu.p@gmail.com>
2021-03-22 00:25:03 +0700

    feat: update main.py

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from src.linemanager import LineManager
+import os
+
+token = 'Y484Uowhl0mb5Bmq3wI4Zgfxia81t4giez8jAQByvmc'
 
 if __name__ == '__main__':
+    messenger = LineManager(token)
+    if os.path.isdir("db"):
+        if os.path.isfile("db/xmarks.csv"):
+            os.remove("db/xmarks.csv")
+    else:
+        os.mkdir("db")
     process = CrawlerProcess(get_project_settings())
     process.crawl("xbot")
     process.start()
+    messenger.xmarkfilter()
+    messenger.xmarktdsum()
+    messenger.xmarkbody()
